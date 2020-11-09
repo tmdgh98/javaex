cd978c4001b6ac348bf176bccd0d493b32b24af3
kawksh <duqrlwjstk@naver.com>
2020-11-09 09:24:43 +0900

    학원

---
+++ src/com/yedam/common/DAO.java
@@ -0,0 +1,37 @@
+package com.yedam.common;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DAO {
+	protected Connection conn;
+	protected Statement stmt;
+	protected PreparedStatement pstmt;
+	protected ResultSet rs;
+	
+	public void connect() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			conn = DriverManager.getConnection(url, "hr", "hr");
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void disconnect() {
+		if(conn!= null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
